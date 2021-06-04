42b5a4703fe53a43d02be5545b9d0f3ac2fbbd2f
saneenKP <saneenkp2000@gmail.com>
2021-06-04 21:04:16 +0530

    Registration Activity Completed

--- app/src/main/java/com/example/wasteit/Register.java
+++ app/src/main/java/com/example/wasteit/Register.java
@@ -1,11 +1,31 @@
 package com.example.wasteit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.wasteit.POJOclasses.UserDetails;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Register extends AppCompatActivity {
 
+
+    private Button register;
+    private EditText name , address , houseNo , email , password , confirmPassword;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
 
 
     @Override
@@ -18,6 +38,106 @@ public class Register extends AppCompatActivity {
             getSupportActionBar().hide();
         }catch (NullPointerException e){}
 
+        register = findViewById(R.id.registerbtn);
+        name = findViewById(R.id.registerName);
+        address = findViewById(R.id.registerAddress);
+        houseNo = findViewById(R.id.registerHouseNo);
+        email = findViewById(R.id.registerEmail);
+        password = findViewById(R.id.registerPassword);
+        confirmPassword = findViewById(R.id.registerConfirmPassword);
+
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (checkFields()){
+                    if (password.getText().toString().equals(confirmPassword.getText().toString())) {
+
+                        UserDetails userDetails = new UserDetails();
+                        userDetails.setName(name.getText().toString());
+                        userDetails.setAddress(address.getText().toString());
+                        userDetails.setEmail(email.getText().toString());
+                        userDetails.setHouseNo(houseNo.getText().toString());
+                        register(email.getText().toString() , password.getText().toString() , userDetails);
+
+                    }else{
+                        Toast.makeText(getApplicationContext() , "Password Does Not Match" , Toast.LENGTH_LONG).show();
+                    }
+                }
+                
+
+            }
+        });
+
+    }
+
+    private boolean checkFields(){
+
+        boolean status = false;
+
+        if (TextUtils.isEmpty(email.getText())){
+            email.setError("email field cannot be empty");
+            status = false;
+        }else if(TextUtils.isEmpty(name.getText())){
+            name.setError("name field cannot be empty");
+            status = false;
+        }else if(TextUtils.isEmpty(address.getText())){
+            address.setError("Address field cannot be empty");
+            status = false;
+        }else if(TextUtils.isEmpty(houseNo.getText())){
+            houseNo.setError("House No is crucial and cannot be empty");
+            status = false;
+        }else if(TextUtils.isEmpty(password.getText())){
+            password.setError("Password field cannot be empty");
+            status = false;
+        }else if(TextUtils.isEmpty(confirmPassword.getText())){
+            confirmPassword.setError(" field cannot be empty");
+            status = false;
+        }else{status = true;}
+
+        return status;
+    }
+
+    private void register(String email , String password , UserDetails userDetails){
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email , password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+
+                    databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
+                    databaseReference.child(firebaseAuth.getCurrentUser().getUid()).setValue(userDetails).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if (task.isSuccessful()){
+
+                                Toast.makeText(getApplicationContext() , "Registration Complete" , Toast.LENGTH_LONG).show();
+                                /*Intent openHome = new Intent();
+                                startActivity(openHome);*/
+
+                            }else
+                            {
+                                Toast.makeText(getApplicationContext() , "Registration Failed : "+task.getException() , Toast.LENGTH_LONG).show();
+
+                            }
+
+                        }
+                    });
+
+
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext() , "Registration Failed : "+task.getException() , Toast.LENGTH_LONG).show();
+
+                }
+                
+            }
+        });
 
     }
 }
