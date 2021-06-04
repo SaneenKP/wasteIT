fe5d9f05927f2743f4cf632a7f1bf461ae13f175
saneenKP <saneenkp2000@gmail.com>
2021-06-04 12:14:01 +0530

    firebase login with email and password implemented

--- app/src/main/java/com/example/wasteit/Login.java
+++ app/src/main/java/com/example/wasteit/Login.java
@@ -1,17 +1,27 @@
 package com.example.wasteit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
     private EditText email , password;
     private Button login , register;
     private String eml , pass;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +32,6 @@ public class Login extends AppCompatActivity {
         try {
             getSupportActionBar().hide();
         }catch (NullPointerException e){}
-
 
         //Attaching Variables with Layout Views
         email = findViewById(R.id.loginEmail);
@@ -51,8 +60,6 @@ public class Login extends AppCompatActivity {
             public void onClick(View v) {
 
 
-
-
             }
         });
 
@@ -60,7 +67,26 @@ public class Login extends AppCompatActivity {
 
     private void login(String email , String pass){
 
+        auth = FirebaseAuth.getInstance();
+        
+        auth.signInWithEmailAndPassword(email , pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()){
+
+                     Intent openHome = new Intent(Login.this , MainActivity.class);
+                    startActivity(openHome);
+                    finish();
+
+                }else
+                {
+                    Toast.makeText(getApplicationContext() , "Login Failed due to  : " + task.getException() , Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
     }
 
-   
 }
