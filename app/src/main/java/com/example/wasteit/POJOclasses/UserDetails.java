5ed9f6003b9ed1f4ed5ab764bf24b0516f76a107
saneenKP <saneenkp2000@gmail.com>
2021-06-04 21:03:48 +0530

    UserDetails POJO  class created

---
+++ app/src/main/java/com/example/wasteit/POJOclasses/UserDetails.java
@@ -0,0 +1,43 @@
+package com.example.wasteit.POJOclasses;
+
+public class UserDetails {
+
+    private String email;
+    private String name;
+    private String HouseNo;
+    private String Address;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHouseNo() {
+        return HouseNo;
+    }
+
+    public void setHouseNo(String houseNo) {
+        HouseNo = houseNo;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+}
+
+
