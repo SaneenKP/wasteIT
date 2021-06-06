b34e1f8ed6a86ef7ca697d8e0600b3facf86a1c9
saneenKP <saneenkp2000@gmail.com>
2021-06-07 03:42:44 +0530

    Date and time picker implemented

--- app/src/main/java/com/example/wasteit/MainActivity.java
+++ app/src/main/java/com/example/wasteit/MainActivity.java
@@ -1,14 +1,127 @@
 package com.example.wasteit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
 
+import android.app.DatePickerDialog;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TimePicker;
+
+import com.example.wasteit.UtilityClasses.DatePickerFragment;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText date , time;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    private ImageButton openProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+            getSupportActionBar().hide();
+        }catch (NullPointerException e){}
+
+        date = findViewById(R.id.date);
+        time = findViewById(R.id.time);
+        disableEditext(date);
+        disableEditext(time);
+
+        openProfile = findViewById(R.id.profileMenu);
+
+
+        openProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this , Profile.class);
+                startActivity(intent);
+
+            }
+        });
+
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                showDatePickerDialog();
+            }
+        });
+
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimePickerDialog();
+            }
+        });
     }
+
+    public void showTimePickerDialog(){
+
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        // Launch Time Picker Dialog
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+
+                        time.setText(hourOfDay + ":" + minute);
+                    }
+                }, mHour, mMinute, false);
+        timePickerDialog.show();
+    }
+
+    public void showDatePickerDialog() {
+
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        date.setText(dayOfMonth + " / " + (monthOfYear+1) + " / " + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+    }
+
+
+
+    private EditText disableEditext(EditText editText){
+
+        editText.setTag(editText.getKeyListener());
+        editText.setKeyListener(null);
+        return editText;
+
+    }
+
 }
