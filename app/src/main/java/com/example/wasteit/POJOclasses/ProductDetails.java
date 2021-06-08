de5acf343300592189c7cb49eb666e1e25a39eb4
saneenKP <saneenkp2000@gmail.com>
2021-06-09 00:27:30 +0530

    Product details POJO class

---
+++ app/src/main/java/com/example/wasteit/POJOclasses/ProductDetails.java
@@ -0,0 +1,52 @@
+package com.example.wasteit.POJOclasses;
+
+public class ProductDetails {
+
+    private String name;
+    private String price;
+    private String points;
+    private String Description;
+    private String image;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        this.Description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
+
+
